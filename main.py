a08dccb6be192a1c0d74a90dcebcb2d732f9388e
immonroe <immanuelmonroe@gmail.com>
2024-08-30 13:12:09 -0400

    Added FPS using pygame functions and dt variable

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from constants import *
 
 def main():
-    
+    # game initialization
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -10,6 +10,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # game loop
     while True:
         screen.fill(0)
         pygame.display.flip()
@@ -17,7 +21,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        # "delta time" - time passed since the last frame was drawn
+        dt =clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
