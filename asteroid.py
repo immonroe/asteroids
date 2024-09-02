26f1bb9b0072fdb19f68e41f2c376e29ecba8e39
immonroe <immanuelmonroe@gmail.com>
2024-09-02 18:21:29 -0400

    Added collision on asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,21 +1,15 @@
 import pygame
+import random
+from constants import *
 from circleshape import CircleShape
 
-class Asteroid(CircleShape, pygame.sprite.Sprite):
-    def __init__(self, x, y, radius, velocity):
 
-        CircleShape.__init__(self, x, y, radius)
-        pygame.sprite.Sprite.__init__(self, self.containers)
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
 
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.velocity = velocity
-
-    def draw(self,screen):
-        position = (self.x, self.y)
-        pygame.draw.circle(screen, "white", position, self.radius, 2)
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        self.x += self.velocity.x * dt
-        self.y += self.velocity.y * dt
+        self.position += self.velocity * dt
--- asteroidfield.py
+++ asteroidfield.py
@@ -33,7 +33,7 @@ class AsteroidField(pygame.sprite.Sprite):
         self.spawn_timer = 0.0
 
     def spawn(self, radius, position, velocity):
-        asteroid = Asteroid(position.x, position.y, radius, velocity)
+        asteroid = Asteroid(position.x, position.y, radius)
         asteroid.velocity = velocity
 
     def update(self, dt):
